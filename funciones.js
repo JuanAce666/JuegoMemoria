517e1d0fb7e4a71a28ab8001d0a5132398f3eae3
JuanPepas <finnbl15yt@gmail.com>
2023-09-13 11:49:46 -0500

    juego completado 80%

--- funciones.js
+++ funciones.js
@@ -53,6 +53,66 @@ let imagenes = [
 let tablero = d.querySelector(".tablero");
 let nombreImg = [];
 let posImg = [];
+let aciertos = 0;
+let intentos = 0;
+let tiempo = 60;
+let mostrarAciertos = d.querySelector(".aciertos");
+let mostrarIntentos = d.querySelector(".intentos");
+let mostrarTiempo = d.querySelector(".tiempo");
+let mostrarNiveles = d.querySelector(".nivel");
+mostrarTiempo.textContent = tiempo;
+let btnIniciar = d.querySelector(".boton-iniciar");
+let juegoActivo = false;
+let nivel = 1;
+let tiempoTrancurrido;
+let soundLoser = new Audio("sonido/loser.mp3");
+
+//desorganizar imagenes
+imagenes.sort(()=> Math.random() - 0.5 );
+
+//iniciar juego
+btnIniciar.addEventListener("click",function () {
+    if (juegoActivo == false && nivel == 1) {
+        juegoActivo = true;
+        agregarImagenes();
+        tiempoDeJuego();
+    }else if (juegoActivo == false && nivel == 2){
+        juegoActivo = true;
+        agregarImagenes();
+        tiempoDeJuego();
+    }else if(juegoActivo == false && nivel == 3){
+        juegoActivo = true;
+        agregarImagenes();
+        tiempo();
+    }
+} );
+
+//tiempo del juego
+function tiempoDeJuego( ) {
+    tiempoTrancurrido = setInterval(function(){
+        tiempo--;
+        mostrarNiveles.textContent = nivel;
+        mostrarTiempo.textContent = tiempo;
+        if(tiempo == 10){
+            //clearInterval(tiempoTrancurrido);
+            mostrarTiempo.setAttribute("style", 
+            "color:red; font-size:20px");
+        }else if(tiempo == 0){
+            
+            clearInterval(tiempoTrancurrido);
+            alert("perdiste :( No adivinaste todas las imagenes");
+           location.reload()
+        }
+    }, 1000);
+}
+
+/*let tiempoTrancurrido = setInterval(function(){
+    if(tiempo == 0){
+        clearInterval(tiempoTrancurrido);
+    }
+    console.log(tiempo--);
+}, 1000);*/
+
 
 //agregar imagenes al tablero
 function agregarImagenes(){
@@ -68,7 +128,7 @@ function agregarImagenes(){
         tablero.appendChild(div);
     }
 }
-agregarImagenes();
+
 //funcion para mostrar las imagenes
 function mostrarImagenes() {
     //guardar ID de la imagen
@@ -85,24 +145,89 @@ function mostrarImagenes() {
     }
 }
 
+
 //funcion para comparar las imagenes
 function comparaImg() {
     let todasImg = d.querySelectorAll(".tablero .col-3 img");
     //comparar imagenes
     if (nombreImg[0] == nombreImg[1]) {
-        todasImg[ posImg[0] ].setAttribute("src","imagenes/ok.jpg");
-        todasImg[ posImg[1] ].setAttribute("src","imagenes/ok.jpg");
+        if (posImg[0] != posImg[1]) {
+            todasImg[ posImg[0] ].setAttribute("src","imagenes/ok.jpg");
+            todasImg[ posImg[1] ].setAttribute("src","imagenes/ok.jpg");
+            todasImg[ posImg[0] ].removeEventListener("click",mostrarImagenes);
+            todasImg[ posImg[1] ].removeEventListener("click",mostrarImagenes);
+            aciertos++
+            mostrarAciertos.textContent = aciertos
+        }else{
+            alert("debes escoger otra imagen");
+            todasImg[posImg[0]].setAttribute("src","imagenes/onepiece1.jpg")
+            intentos++
+            mostrarIntentos.textContent = intentos;
+            soundLoser.play();
+        }
+    
     }else{
         todasImg[ posImg[0] ].setAttribute("src","imagenes/onepiece1.jpg");
         todasImg[ posImg[1] ].setAttribute("src","imagenes/onepiece1.jpg");
+        intentos++;
+        mostrarIntentos.textContent = intentos;
     }
     nombreImg= [];
     posImg= [];
+
+    //Logica pasar de niveles 
+    if(aciertos == 6 && nivel == 1){
+        alert("🌟felicidades pasaste al siguiente nivel🌟");
+        aciertos = 0;
+        intentos = 0;
+        clearInterval(tiempoTrancurrido);
+        tiempo = 45;
+        nivel = 2;
+        mostrarNiveles.textContent = nivel;
+        mostrarIntentos.textContent = intentos;
+        mostrarAciertos.textContent = aciertos;
+        mostrarTiempo.textContent = tiempo;
+        quitarImagenes();
+        juegoActivo = false;
+    }else if(aciertos == 6 && nivel == 2) {
+        alert("🌟felicidades pasaste al siguiente nivel🌟");
+        aciertos = 0;
+        intentos = 0;
+        clearInterval(tiempoTrancurrido);
+        tiempo = 35;
+        nivel = 3;
+        mostrarNiveles.textContent = nivel;
+        mostrarIntentos.textContent = intentos;
+        mostrarAciertos.textContent = aciertos;
+        mostrarTiempo.textContent = tiempo;
+        quitarImagenes();
+        juegoActivo = false;
+    }else if (aciertos == 6 && nivel == 3){
+        alert("🌟felicidades has superado todos los  nivel🌟");
+        aciertos = 0;
+        intentos = 0;
+        clearInterval(tiempoTrancurrido);
+        tiempo = 35;
+        nivel = 1;
+        mostrarNiveles.textContent = nivel;
+        mostrarIntentos.textContent = intentos;
+        mostrarAciertos.textContent = aciertos;
+        mostrarTiempo.textContent = tiempo;
+        quitarImagenes();
+        juegoActivo = false;
+    }
+}
+
+//quitar imagenes del tablero
+function quitarImagenes() {
+    let todasLasImg = d.querySelectorAll(".tablero div");
+    for(let i = 0; i < todasLasImg.length; i++){
+        todasLasImg[i].remove();
+    }
 }
 
 
-
-
+//que toca que hacer nosotros poner mas niveles, que le pida el usuario el nombre y que guarde en el puesto jugador tiempo juego intentos totales
 
 /*もう一つの問題が解決されました
 let tablero = d.querySelector(".tablero");
